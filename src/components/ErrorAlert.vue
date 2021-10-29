0ece273413ad01b1cceb997446579794af89b9fc
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2021-10-29 23:58:44 +0530

    feat: 🎸 error screen using slots

---
+++ src/components/ErrorAlert.vue
@@ -0,0 +1,16 @@
+<template>
+  <dialog open>
+    <slot> </slot>
+  </dialog>
+</template>
+
+<style scoped>
+dialog {
+  margin: 0;
+  position: fixed;
+  top: 20vh;
+  left: 30%;
+  background: #fff;
+  padding: 1rem;
+}
+</style>
--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,4 +1,33 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text" name="" id="" />
+  <input type="text" ref="goal" />
+  <button @click="submit">new goal</button>
+  <error-alert v-if="isError">
+    <h2>Input is empty</h2>
+    <p>Please enter valid text</p>
+    <button @click="close">close</button>
+  </error-alert>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      isError: false,
+    };
+  },
+  methods: {
+    submit() {
+      const val = this.$refs.goal.value;
+      if (val === "") {
+        this.isError = true;
+      }
+    },
+    close() {
+      this.isError = false;
+    },
+  },
+};
+</script>
