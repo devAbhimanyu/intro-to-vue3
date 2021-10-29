773ffff4f014dea5aae0335aa7d993fe4f14a4f3
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2021-10-29 22:31:56 +0530

    refactor: 💡 defined global and local comps

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <Header />
+    <BadgeList />
+    <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    />
   </div>
 </template>
 
 <script>
+import Header from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+import BadgeList from "./components/BadgeList.vue";
+
 export default {
+  /**
+   * local component reg
+   */
+  components: {
+    Header,
+    UserInfo,
+    BadgeList,
+  },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
 
-import App from './App.vue';
-import TheHeader from './components/TheHeader.vue';
-import BaseBadge from './components/BaseBadge.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import App from "./App.vue";
+import BaseBadge from "./components/BaseBadge.vue";
 
 const app = createApp(App);
+/**
+ * global component
+ */
+app.component("base-badge", BaseBadge);
 
-app.component('the-header', TheHeader);
-app.component('base-badge', BaseBadge);
-app.component('badge-list', BadgeList);
-app.component('user-info', UserInfo);
-
-app.mount('#app');
+app.mount("#app");
